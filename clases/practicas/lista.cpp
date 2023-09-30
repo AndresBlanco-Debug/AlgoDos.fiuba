91787db1b9fa2b944f3167e5fcd3203362a93c66
JoseBlanco <jblanco@fi.uba.ar>
2023-09-30 02:00:44 -0300

    creacion de nuevos metodos

--- clases/practicas/lista.cpp
+++ clases/practicas/lista.cpp
@@ -24,26 +24,6 @@ void Lista::altaFinal(int elemento){
 void Lista::altaPrincipio(int elemento){
     punteroLista = new Nodo(elemento, punteroLista);
 }
-//FUNCION ORIGINAL.
-void Lista::altaXPosicion(int elemento, int posicion){
-    Nodo *punteroAux = punteroLista;
-    int contador = 0;
-    //CASo 1: LA LISTA TIENE ELEMENTOS
-    //Este ciclo pide que el puntero exista, recorre todos los nodos hasta llegar a la posicion
-    while((punteroAux) && (punteroAux -> getSigNodo()) && (contador < (posicion - 1))){
-        punteroAux = punteroAux -> getSigNodo();
-        contador++;
-    }
-    if(punteroAux){
-        Nodo *nuevoNodo = new Nodo(elemento, punteroAux -> getSigNodo());
-        punteroAux -> setSigNodo(nuevoNodo);
-    }
-    //CASO QUE LA LISTA ESTE VACIA
-    else{
-        punteroLista = new Nodo(elemento, punteroLista);
-    }
-}
-//
 void Lista::bajaLista(int elemento){
     if(punteroLista){
         if((punteroLista->getInfo() != elemento) && (punteroLista -> getSigNodo())){
@@ -82,6 +62,100 @@ void Lista::emiteLista(){
     while(punteroAux){
         cout << punteroAux -> getInfo();
         punteroAux = punteroAux -> getSigNodo();
+    }
+}
+//FUNCIONES ORIGINALES:::
+void Lista::altaXPosicion(int elemento, int posicion){
+    Nodo *punteroAux = punteroLista;
+    int contador = 0;
+    //CASo 1: LA LISTA TIENE ELEMENTOS
+    //Este ciclo pide que el puntero exista, recorre todos los nodos hasta llegar a la posicion
+    while((punteroAux) && (punteroAux -> getSigNodo()) && (contador < (posicion - 1))){
+        punteroAux = punteroAux -> getSigNodo();
+        contador++;
+    }
+    if(punteroAux){
+        Nodo *nuevoNodo = new Nodo(elemento, punteroAux -> getSigNodo());
+        punteroAux -> setSigNodo(nuevoNodo);
+    }
+        //CASO QUE LA LISTA ESTE VACIA
+    else{
+        punteroLista = new Nodo(elemento, punteroLista);
+    }
+}
+//
+void Lista::bajaPricipio() {
+    //Hay que evaluar dos casos.
+    //caso 1: la lista tiene elementos
+    //caso 2: la lista esta vacia, entonces no hay nada que eliminar
+    if(punteroLista){
+        //Si hay nodos existentes entonces hay elementos en la lista
+        //No hace falta recorrer toda la lista porque eliminamos el primer elemento atado al primer nodo.
+        Nodo *pAux = punteroLista;
+        delete(pAux);
+    }
+    else{
+        cout << "\nActualmente no hay elementos en la lista, intente nuevamente\n" << endl;
+    }
+}
+//
+void Lista::bajaFinal() {
+    //CASO 1: la lista tiene elementos
+    Nodo *punteroAux = punteroLista, *punteroAnterior = punteroAux;
+    while((punteroAux) && (punteroAux -> getSigNodo())){
+        punteroAux = punteroAux -> getSigNodo();
+    }//cuando se borra el nodo auxiliar
+    if(punteroAux){
+        punteroAnterior -> setSigNodo(0);
+        delete punteroAux;
+    }
+    else{
+        delete punteroLista;
+        punteroLista = nullptr;
+    }
+}
+//
+void Lista::bajaXPosicion(int posicion) {
+    Nodo *punteroAux = punteroLista, *nodoPrevio = punteroAux;
+    int indice = 0;
+    //Primero evaluamos el caso si esta en la primera posicion para evitar problemas.
+    if(posicion > 0) {
+        while ((punteroAux) && (punteroAux->getSigNodo()) && indice < (posicion - 1)) {
+            //se recorre la lista hasta llegar a la posicion deseada o hasta que la lista se acabe en caso de pos > topeLista
+            punteroAux = punteroAux->getSigNodo();
+            indice++;
+        }
+        //CASO 1: es el ultimo elemento de la lista
+        if ((punteroAux) && !(punteroAux->getSigNodo())) {
+            bajaFinal();
+        }
+            //CASO 2: esta en otra posicion
+        else if ((punteroAux) && (punteroAux->getSigNodo())) {
+            //solo hay que actualizar el nodo anterior
+            nodoPrevio->setSigNodo(punteroAux);
+            delete punteroAux;
+        }
+    }
+    else if(posicion == 0){
+        bajaPricipio();
+    }
+    else{
+        cout << "Error! No se aceptan posiciones negativas." << endl;
+    }
+}
+//
+void Lista::eliminarDato(int elemento) {
+    Nodo *puntAux = punteroLista, *nodoAnterior;
+    //primero hay que comprobar que el dato esta en la lista
+    //se recorre la lista para saber la cantidad de elementos
+    if(datoEnLista(elemento)){
+        while(puntAux){
+            if(puntAux->getInfo() == elemento){
+                nodoAnterior ->setSigNodo(puntAux->getSigNodo());
+            }
+
+
+        }
     }
 }
 //
--- clases/practicas/lista.h
+++ clases/practicas/lista.h
@@ -8,11 +8,26 @@ class Lista
     public:
         Lista();
         void altaFinal(int);
+        //Ingresa datos al final de la lista
         void altaPrincipio(int);
+        //Ingresa datos al principio de la lista
         void altaXPosicion(int,int);
+        //Ingresa datos en cualquier posicion de la lista
         void bajaLista(int);
+        //Elimina el primer elemento que coincida con el ingresado
         bool datoEnLista(int);
+        //revisa si un dato ya esta en la lista
         void emiteLista();
+        //muestra la lista
+        void bajaPricipio();
+        //elimina el primer elemento de la lista
+        void bajaFinal();
+        //elimina el ultimo elemento de la lista
+        void bajaXPosicion(int);
+        //elimina el elemento en cualquier posicion
+        void eliminarDato(int);
+        //elimina todas las apariciones del mismo elemento en la lista
+
         ~Lista();
 };
 
